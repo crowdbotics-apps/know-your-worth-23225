4af98a91d32aa6fd2295089e430f87eef0482da7
Crowdbotics <team@crowdbotics.com>
2020-12-07 00:52:31 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/know_your_worth_23225/MainActivity.java
@@ -0,0 +1,15 @@
+package com.know_your_worth_23225;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "know_your_worth_23225";
+    }
+}
---
+++ backend/know_your_worth_23225/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for know_your_worth_23225 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'know_your_worth_23225.settings')
+
+application = get_wsgi_application()
